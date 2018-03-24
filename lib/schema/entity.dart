677d50f0ce9302de77896ce65ec1d693e0b29c74
ivanserbyniuk <ivan.serbyniuk@gmail.com>
2018-03-24 11:05:04 +0200

    Add tests

--- lib/schema/entity.dart
+++ lib/schema/entity.dart
@@ -1,42 +1,5 @@
 import 'package:dart_normalizer/schema/immutable_utils.dart' as ImmutableUtils;
 
-/*
-
-var getDefaultGetId = (idAttribute) => (input) => input[idAttribute];
-class Entity {
-
-  String key;
-  var idAttribute = 'id';
-  var getDefaultGetId = (idAttribute) => (input) => input[idAttribute];
-  //Map<String, dynamic> schema = {};
-  var _processStrategy = (input, parent, key) => [input];
-
-  getId(input, parent, key) => input[idAttribute];//getDefaultGetId(idAttribute);
-
-
-
-  Entity(this.key, {String idAttribute ,definition, options}){
-    if(idAttribute != null) {
-      this.idAttribute = idAttribute;
-    }
-  }
-
-  normalize(Map input, parent, key, visit, addEntity) {
-    final processedEntity = input;
-    if (this.key == key) {
-      var schema = key;
-      processedEntity[key] = visit(
-          processedEntity[key], processedEntity, key, schema, addEntity);
-      print("prossesed entity $processedEntity");
-    }
-
-    addEntity(this, processedEntity, input, parent, key);
-
-    return input[idAttribute];
-  }
-
-}
-*/
 
 class EntitySchema {
   String key;
@@ -47,10 +10,9 @@ class EntitySchema {
   var schema;
 
 
-  var getDefaultGetId = (idAttribute) {
-   return (input) => input[idAttribute];};
+  var getDefaultGetId = (idAttribute) => (input, paremt, key) => input[idAttribute];
 
-  EntitySchema(this.key, {definition, options, idAttribute, idAttributeFun}) {
+  EntitySchema(this.key, {definition, options, idAttribute}) {
     if (key == null) {
       throw new Exception();
     }
@@ -59,19 +21,14 @@ class EntitySchema {
       return {entityA: [entityA], entityA: [entityB]};
     };
     var processStrategy = (input) => ([input]);
-    this.idAttribute = "id";
-
-    if (idAttribute != null) {
-      _getId = getDefaultGetId(idAttribute);
-      this.idAttribute = idAttribute;
-    }
-    else if (idAttributeFun != null) {
-      _getId = idAttributeFun;
-     // idAttribute = idAttributeFun;
+   if (idAttribute == null) {
+      // this.idAttribute = "id";
+       this._getId = getDefaultGetId("id");
     }
     else {
-      print(idAttribute);
-      _getId = getDefaultGetId(this.idAttribute);
+      this._getId = idAttribute is String
+          ? getDefaultGetId(idAttribute)
+          : idAttribute;
     }
     this._mergeStrategy = mergeStrategy;
     this._processStrategy = processStrategy;
@@ -79,11 +36,11 @@ class EntitySchema {
   }
 
 
-  define(definition) {
+  define(Map definition) {
     if (definition != null) {
-      this.schema = (definition.keys).reduce((entitySchema, key) {
+      this.schema = definition.map((key, value) {
         final schema = definition[key];
-        return { "entitySchema": entitySchema, "key": schema};
+        return MapEntry(key, schema);
       });
     }
     if (schema == null) {
@@ -98,35 +55,22 @@ class EntitySchema {
 
   normalize(input, parent, key, visit, addEntity) {
     final processedEntity = input; // this._processStrategy(input, parent, key);
-    (this.schema.keys).forEach((key) {
+    /*(this.schema.keys).forEach((key) {
       if (processedEntity.containsKey(key) && processedEntity[key] is Map) {
         final schema = this.schema[key];
         processedEntity[key] = visit(
             processedEntity[key], processedEntity, key, schema, addEntity);
       }
-    });
+    });*/
     addEntity(this, input, input, parent, key);
     print("normalize");
     return getId(input, parent, key);
   }
 
   getId(input, parent, key) {
-    return this._getId(input);//, parent, key);
+    return this._getId(input, parent, key);
   }
 
-/* denormalize(entity, unvisit) {
-    if (ImmutableUtils.isImmutable(entity)) {
-      return ImmutableUtils.denormalizeImmutable(this.schema, entity, unvisit);
-    }
-
-    Object.keys(this.schema).forEach((key) => {
-        if (entity.hasOwnProperty(key)) {
-    const schema = this.schema[key];
-    entity[key] = unvisit(entity[key], schema);
-    }
-  });
-    return entity;
-  }*/
 }
 
 
--- test/entity.dart
+++ test/entity.dart
@@ -52,7 +52,7 @@ void main() {
 
   test('can normalize entity IDs based on their object key', () {
     //todo int string key check
-    var expectedJson= """
+    var expectedJson = """
     {
   "entities": {
     "users": {
@@ -76,16 +76,16 @@ void main() {
   }
 }""";
     var user = new EntitySchema(
-        'users', idAttributeFun: (entity, parent, key) => key);
+        'users', idAttribute: (entity, parent, key) => key);
     var inputSchema = new Values(
         { "users": user}, schemaAttribute: (input, parent, key) => 'users');
 
-    var input = { 4: { "name": 'taco'}, 56: { "name": 'burrito'}};
+    var input = { '4': { "name": 'taco'}, '56': { "name": 'burrito'}};
     expect(normalize(input, inputSchema), fromJson(expectedJson));
   });
 
 
-  test('can build the entity\'s ID from the parent object', () {
+  test('can build the entitys ID from the parent object', () {
     var expectedJson = """ 
     {
   "entities": {
@@ -101,15 +101,65 @@ void main() {
     "user": "tacos-user-4"
   }
 }""";
-      var user = new EntitySchema('users',
-        idAttributeFun: (entity, parent, key) => "${parent.name}-${key}-${entity.id}"
-      );
-  var inputSchema = new ObjectSchema({"user": user });
+    var user = new EntitySchema('users',
+        idAttribute: (entity, parent,
+            key) => "${parent['name']}-${key}-${entity['id']}"
+    );
+    var inputSchema = new ObjectSchema({"user": user});
 
-  var input = { "name": 'tacos', "user": { "id": '4', "name": 'Jimmy' } };
-  expect(normalize(input, inputSchema),fromJson(expectedJson));
-});
+    var input = { "name": 'tacos', "user": { "id": '4', "name": 'Jimmy'}};
+    expect(normalize(input, inputSchema), fromJson(expectedJson));
+  });
+
+  ///merge strategy, proccess strategy
+  ///
+  ///
+  /// denormalizetion
+  test('denormalizes an entity', () {
+    var expectedJson = """ {
+      "id": 1,
+      "type": "foo"
+    }""";
+    var mySchema = new EntitySchema('tacos');
+    const entities = {
+      "tacos": {
+        1: { "id": 1, "type": 'foo'}
+      }
+    };
+    expect(denormalize(1, mySchema, entities), fromJson(expectedJson));
+  });
+
+  test('denormalizes deep entities', () {
+    var expectedJson1 = """ {
+    "food": {
+      "id": 1
+   },
+  " id": 1
+  }""";
+
+    var expectedJson2 = """  {
+      "id": 2
+    }""";
+    var foodSchema = new EntitySchema('foods');
+    var menuSchema = new EntitySchema('menus', definition: {
+      "food": foodSchema
+    });
+
+    const entities = {
+      "menus": {
+        1: { "id": 1, "food": 1},
+        2: { "id": 2}
+      },
+      "foods": {
+        1: { "id": 1}
+      }
+    };
+
+    expect(denormalize(1, menuSchema, entities), expectedJson1);
+    expect(denormalize(2, menuSchema, entities), expectedJson2);
+  });
 }
+
 
 toJson(Map value) {
   JsonEncoder encoder = new JsonEncoder.withIndent('');
